13aa167d7f7428d139086ed909292521827d650d
helloZw <466747863@qq.com>
2020-03-26 16:32:56 +0800

    mybatis通用反向工程

--- src/main/java/com/zw/mybatisgenerator/MybatisGeneratorApplication.java
+++ src/main/java/com/zw/mybatisgenerator/MybatisGeneratorApplication.java
@@ -1,4 +1,4 @@
-package com.lwh.mybatisgenerator;
+package com.zw.mybatisgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
